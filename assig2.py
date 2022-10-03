b7e91016920d2f3e6209c8be6d72628861cda53a
Karina Puente <puentek@oregonstate.edu>
2022-10-03 10:06:07 -0700

    changes have been made/added

---
+++ assig2.py
@@ -0,0 +1,15 @@
+
+'''1. Prompt the user to enter a numerical limit. (Recall the input function from chapter 1)
+   2. For the numbers 1 up to the number the user entered, the following should happen.
+        1. Always print out the current number
+        2.If the number is divisible by 3 also print "Fizz"
+        3. If the number is divisible by 5 also print "Buzz"
+        4.If the number is divisible by 3 and 5 also print "Fizz Buzz"
+   3.Once you have finished all the numbers output "Done!"
+
+    **You will write two different versions of this assignment, one accomplishes the task using a "while" loop, the other uses a "for" loop. '''
+
+
+print("Enter a nummber: ")
+num = str(input())
+
