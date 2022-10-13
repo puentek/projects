04cd73a3c507156b44fd089d70ac4f99a58fec99
Karina Puente <puentek@oregonstate.edu>
2022-10-12 22:20:07 -0700

    all parts have been finished

--- abs_sort.py
+++ abs_sort.py
@@ -1,13 +1,10 @@
-
+# Created by Karina Puente Oct 2022
 
 ''' Implementation of teo different functions: 
 1. abs_sort(list) : This takes in an unsorted list and returns a sorted list according to the absolute sorting rules detailed above. The original list which is passed in should be unchanged 
 2. abs_sort_in_place(list): This performs the same sort but does so in place. This means that the original list, after the function is complete, should now be sorted. It does not return anything, the original list is the one that ends up sorted.'''
 
 
-# list = [5, -2, 10, -13, 2, 20]
-
-# def swap_num(num1, num2):
 
 def abs_sort(list):
     new_list = list 
@@ -24,12 +21,15 @@ def abs_sort(list):
             
             else: 
                 ""
+    # I'm just printing this out for my sanity 
     print(f'This is the abs_sort new_list: {new_list}')
     return new_list
 # This is the original list that will be passed through the function 
 list = [5, -2, 10, -13, 2, 20]
 abs_sort(list)
 
+
+# This is the second part of the assignment 
 def abs_sort_in_place(org_list):
     # org_list = org_list
     for i in range(len(org_list)-1):
@@ -45,9 +45,9 @@ def abs_sort_in_place(org_list):
             
             else: 
                 ""
-
+    # I'm just printing this out for my sanity 
     print(f'This is the abs_sort_in_place(org_list): {org_list}')
     return org_list
-
+# this is the original list
 org_list = [5, -2, 10, -13, 2, 20]
 abs_sort_in_place(org_list)
